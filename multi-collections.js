f06a3bcd7bd1704e6c66fa71dcd0ab96c25fa31c
svasizeswar <vasi@exemplifi.io>
2022-04-21 18:53:11 +0530

    same

--- multi-collections.js
+++ multi-collections.js
@@ -1,27 +1,3 @@
-Vue.component('post-item', {
-template: `<div class="section bg-blue py-60 wf-section">
-		<div class="container-2 w-container">
-		<div class="text-center text-blue-medium uppercase text-bold-700">Resources</div>
-		<h2 class="text-white text-center mb-48 mt-10">Moloco recent events</h2>
-		<div class="px-0"></div>
-			<div class="w-dyn-list">
-			<div role="list" class="collection-list w-dyn-items w-row">
-				<div role="listitem" class="collection-item w-dyn-item w-col w-col-4" v-for="(entry, index) in items"  v-bind:item="entry.id" v-bind:key="index">
-				<div class="bg-white">
-				<img :src= "entry.image" loading="lazy" alt="" sizes="(max-width: 444px) 87vw, (max-width: 767px) 387px, 31vw" >
-				<div class="p-20 py-12"><div class="text-14 text-blue text-bold-700 uppercase my-8">{{entry.name}}</div>
-				<p class="text-blue-medium line-height-1-3">Moloco Unveils New Campaign Setup UI for Moloco Cloud DSP</p>
-									</div>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>`,
-props: ['items' ]
-});
-							  
-
 var app = new Vue({
 	el: '#app',
 	data: {
@@ -34,23 +10,23 @@ var app = new Vue({
 			let shouldSkip = false;
 			const self = this;
 			data.items.forEach(function(item, index) {
-				var name = item.name;
-
-				if (shouldSkip) {
-					return;
-				}
-				if (index >= 1) {
-					shouldSkip = true;
-					return;
-				} 
-				self.items.push({
-					'name': name,
-					'image': item['image'].url,
-					'author': item.author,
-					'id': item._id,
-					'cid': item._cid,
-					'slug': '/case-studies/'+item.slug
-				});
+				var name = item.name; 
+					if (shouldSkip) {
+						return;
+					}
+					if (index >= 1) {
+						shouldSkip = true;
+						return;
+					} 
+					self.items.push({
+						'name': name,
+						'image': item['image'].url,
+						'author': item.author,
+						'id': item._id,
+						'cid': item._cid,
+						'slug': '/case-studies/'+item.slug
+					});
+				 
 			});											 
 		},
 		getBlogs: function(data) { 
@@ -82,7 +58,7 @@ var app = new Vue({
 			const self = this;
 			data.items.forEach(function(item, index) {
 				var name = item.name;
-
+				 
 				if (shouldSkip) {
 					return;
 				}
@@ -112,6 +88,14 @@ var app = new Vue({
 				return results
 			})
 			.catch(error => { console.log(error) });										
+		},
+		filteredRecipes: function(data) {	 
+			
+			data.items = data.items.filter((item) => {
+			  return (item.featured == true)
+			})	
+			console.log(data);										
+			return data;
 		}
 	},
 	created: function() {
@@ -126,10 +110,14 @@ var app = new Vue({
 
 		Promise.all(endpoints.map((endpoint) => axios.get(endpoint))).then(
 		  axios.spread((case_studies, blogs, rnd_blogs) => {
-				this.getCaseStudies(case_studies.data);
-				this.getBlogs(blogs.data);
+		 // console.log(case_studies.data);
+				var casestudy_data = this.filteredRecipes(case_studies.data);
+				var blog_data = this.filteredRecipes(blogs.data);
+				var rnd_blog_data = this.filteredRecipes(rnd_blogs.data);
+				this.getCaseStudies(casestudy_data);
+				this.getBlogs(blog_data);
 				this.getRNDBlogs(rnd_blogs.data);
 		  })
 		);
 	}
-})	
+})		
